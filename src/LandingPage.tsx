6f4224989ec1ffb4ba53df389972b18ae2c2cbd8
Ari-Tech <arindamTech@outlook.com>
2019-12-23 14:36:04 +0800

    added left NavBar and main container basic css added

---
+++ src/LandingPage.tsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+
+const LandingPage: React.FC = ()=>{
+    return (
+        <div className={'container-fluid'}>
+            <div className={'row'}>
+                <div className={'left-menu'}>
+                    Left menu
+                </div>
+                <div className={'col main-container'}>
+                    Main Container
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export {LandingPage}
